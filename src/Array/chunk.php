b36171a9c0fa1d8688ba988e790afd4ec019d7c2
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-14 15:22:10 +0200

    Add array.chunk function

---
+++ src/Array/chunk.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Creates an array of elements split into groups the length of `size`.
+ * If `array` can't be split evenly, the final chunk will be the remaining
+ * elements.
+ *
+ * @category Array
+ *
+ * @param array $array  array The array to process.
+ * @param int   $number [size=1] The length of each chunk
+ *
+ * @return array Returns the new array of chunks.
+ * @example
+ *
+ * chunk(['a', 'b', 'c', 'd'], 2)
+ * // => [['a', 'b'], ['c', 'd']]
+ *
+ * chunk(['a', 'b', 'c', 'd'], 3)
+ * // => [['a', 'b', 'c'], ['d']]
+ */
+function chunk(?array $array, int $number): array
+{
+    if ($number < 1) {
+        return [];
+    }
+
+    return \array_chunk($array ?? [], $number, false);
+}
---
+++ tests/Array/ChunkTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the lodash-php project.
+ *
+ * @author     pierre
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\chunk;
+
+class ChunkTest extends TestCase
+{
+    public function testChunk()
+    {
+        $this->assertSame([['a', 'b'], ['c', 'd']], chunk(['a', 'b', 'c', 'd'], 2));
+        $this->assertSame([['a', 'b', 'c'], ['d']], chunk(['a', 'b', 'c', 'd'], 3));
+        $this->assertSame([], chunk(null, 0));
+        $this->assertSame([], chunk([], 0));
+        $this->assertSame([], chunk([], -12));
+        $this->assertSame([], chunk([], 22));
+    }
+}
