b8c8a0a664138e3a20e84c8ed6d6f599eea47d49
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-14 15:22:40 +0200

    Add array.compact function

---
+++ src/Array/compact.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Creates an array with all falsey values removed. The values `false`, `null`,
+ * `0`, `""`, `undefined`, and `NaN` are falsey.
+ *
+ * @since    0.1.0
+ * @category Array
+ *
+ * @param array $array The array to compact.
+ *
+ * @return array Returns the new array of filtered values.
+ * @example
+ *
+ * compact([0, 1, false, 2, '', 3])
+ * // => [1, 2, 3]
+ */
+function compact(?array $array): array
+{
+    return \array_values(\array_filter($array ?? []));
+}
---
+++ tests/Array/CompactTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the lodash-php project.
+ *
+ * @author     pierre
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\compact;
+
+class CompactTest extends TestCase
+{
+    public function testChunk()
+    {
+        $this->assertSame([1, 2, 3], compact([0, 1, false, 2, '', 3]));
+        $this->assertSame([], compact([]));
+        $this->assertSame([], compact(null));
+    }
+}
