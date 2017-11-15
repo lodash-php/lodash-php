85cdced528f0a3fe2e2a815b7b56160c95da6ac9
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-15 07:33:50 +0200

    Add array.differenceWith function

---
+++ src/Array/differenceWith.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+use function _\internal\baseFlatten;
+
+/**
+ * This method is like `difference` except that it accepts `comparator`
+ * which is invoked to compare elements of `array` to `values`. The order and
+ * references of result values are determined by the first array. The comparator
+ * is invoked with two arguments: (arrVal, othVal).
+ *
+ * **Note:** Unlike `pullAllWith`, this method returns a new array.
+ *
+ * @category Array
+ *
+ * @param array    $array      The array to inspect.
+ * @param array... $values The values to exclude.
+ * @param callable $comparator The comparator invoked per element.
+ *
+ * @return array Returns the new array of filtered values.
+ * @example
+ * <code>
+ * $objects = [[ 'x' => 1, 'y' => 2 ], [ 'x' => 2, 'y' => 1 ]]
+ *
+ * differenceWith($objects, [[ 'x' => 1, 'y' => 2 ]], '_::isEqual')
+ * // => [[ 'x' => 2, 'y' => 1 ]]
+ * </code>
+ */
+function differenceWith(array $array, ...$values): array
+{
+    if (!$array) {
+        return [];
+    }
+
+    if (!\is_callable(\end($values))) {
+        return difference($array, ...$values);
+    }
+
+    $comparator = \array_pop($values);
+
+    $values = baseFlatten($values, 1, 'is_array', true, null);
+
+    $valuesLength = \count($values);
+    $result = [];
+
+    foreach ($array as $value) {
+        $valuesIndex = $valuesLength;
+        while ($valuesIndex--) {
+            if ($comparator($value, $values[$valuesIndex])) {
+                continue 2;
+            }
+        }
+
+        $result[] = $value;
+    }
+
+    return $result;
+}
---
+++ tests/Array/DifferenceWithTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\differenceWith;
+
+class DifferenceWithTest extends TestCase
+{
+    public function testDifferenceWith()
+    {
+        $objects = [[ 'x' => 1, 'y' => 2 ], [ 'x' => 2, 'y' => 1 ]];
+
+        $this->assertSame([[ 'x' => 2, 'y' => 1 ]], differenceWith($objects, [[ 'x' => 1, 'y' => 2 ]], '_\isEqual'));
+    }
+}
