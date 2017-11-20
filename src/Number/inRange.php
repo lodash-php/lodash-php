acd5d9e3e1b646b19f7be2b11d1592db0f942eee
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-20 14:42:36 +0200

    Add Number.inRange function

---
+++ src/Number/inRange.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Checks if `number` is between `start` and up to, but not including, `end`. If
+ * `end` is not specified, it's set to `start` with `start` then set to `0`.
+ * If `start` is greater than `end` the params are swapped to support
+ * negative ranges.
+ *
+ * @category Number
+ *
+ * @param float number The number to check.
+ * @param float $start The start of the range.
+ * @param float $end   The end of the range.
+ *
+ * @return boolean Returns `true` if `number` is in the range, else `false`.
+ * @see      range, rangeRight
+ * @example
+ * <code>
+ * inRange(3, 2, 4)
+ * // => true
+ *
+ * inRange(4, 8)
+ * // => true
+ *
+ * inRange(4, 2)
+ * // => false
+ *
+ * inRange(2, 2)
+ * // => false
+ *
+ * inRange(1.2, 2)
+ * // => true
+ *
+ * inRange(5.2, 4)
+ * // => false
+ *
+ * inRange(-3, -2, -6)
+ * // => true
+ * </code>
+ */
+function inRange(float $number, float $start = 0, float $end = 0): bool
+{
+    if (0 === $end) {
+        $end = $start;
+        $start = 0;
+    }
+
+    return $number >= \min($start, $end) && $number < \max($start, $end);
+}
---
+++ tests/Number/InRangeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\inRange;
+
+class InRangeTest extends TestCase
+{
+    /**
+     * @throws \PHPUnit\Framework\AssertionFailedError
+     */
+    public function testInRange()
+    {
+        $this->assertTrue(inRange(3, 2, 4));
+        $this->assertTrue(inRange(4, 8));
+        $this->assertFalse(inRange(4, 2));
+        $this->assertFalse(inRange(2, 2));
+        $this->assertTrue(inRange(1.2, 2));
+        $this->assertFalse(inRange(5.2, 4));
+        $this->assertTrue(inRange(-3, -2, -6));
+    }
+}
