e4d7388b54760017be5434ec5b44d485e7fd1f8d
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-22 17:36:22 +0200

    Add Array.dropRightWhile function

---
+++ src/Array/dropRightWhile.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Creates a slice of `array` excluding elements dropped from the end.
+ * Elements are dropped until `predicate` returns falsey. The predicate is
+ * invoked with three arguments: (value, index, array).
+ *
+ * @category Array
+ *
+ * @param array    $array     The array to query.
+ * @param callable $predicate The function invoked per iteration.
+ *
+ * @return array the slice of `array`.
+ * @example
+ * <code>
+ * $users = [
+ *   [ 'user' => 'barney',  'active' => false ],
+ *   [ 'user' => 'fred',    'active' => true ],
+ *   [ 'user' => 'pebbles', 'active' => true ]
+ * ]
+ *
+ * dropRightWhile($users, function($user) { return $user['active']; })
+ * // => objects for ['barney']
+ * </code>
+ */
+function dropRightWhile(array $array = null, callable $predicate = null): array
+{
+    \end($array);
+    $length = \count($array);
+    $index = \key($array);
+    while ($length && $predicate($array[$index], $index, $array)) {
+        array_pop($array);
+        $length--;
+        \end($array);
+        $index = \key($array);
+    }
+
+    return $array;
+}
---
+++ tests/Array/DropRightWhileTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\dropRightWhile;
+
+class DropRightWhileTest extends TestCase
+{
+    public function testDropRightWhile()
+    {
+        $users = [
+            ['user' => 'barney', 'active' => false],
+            ['user' => 'fred', 'active' => true],
+            ['user' => 'pebbles', 'active' => true],
+        ];
+
+        $this->assertSame([[ 'user' => 'barney',  'active' => false ]], dropRightWhile($users, function($user) { return $user['active']; }));
+    }
+}
