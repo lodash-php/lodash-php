9170cd4d16d2115bdeba220c984fec2bd206f17b
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-28 20:56:47 +0200

    Add Array.findIndex function

---
+++ src/Array/findIndex.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+use function _\internal\baseIteratee;
+
+/**
+ * This method is like `find` except that it returns the index of the first element predicate returns truthy for instead of the element itself.
+ *
+ * @category Array
+ *
+ * @param array    $array     The array to inspect.
+ * @param callable $predicate The function invoked per iteration.
+ * @param int      $fromIndex The index to search from.
+ *
+ * @return int the index of the found element, else `-1`.
+ * @see      find, findKey, findLast, findLastKey, findLastIndex
+ * @example
+ * <code>
+ * $users = [
+ *     ['user' => 'barney',  'active' => false],
+ *     ['user' => 'fred',    'active' => false],
+ *     ['user' => 'pebbles', 'active' => true],
+ * ];
+ *
+ * findIndex($users, function($o) { return $o['user'] s== 'barney'; });
+ * // => 0
+ *
+ * // The `matches` iteratee shorthand.
+ * findIndex($users, ['user' => 'fred', 'active' => false]);
+ * // => 1
+ *
+ * // The `matchesProperty` iteratee shorthand.
+ * findIndex($users, ['active', false]);
+ * // => 0
+ *
+ * // The `property` iteratee shorthand.
+ * findIndex($users, 'active');
+ * // => 2
+ * </code>
+ */
+function findIndex(array $array, $predicate, int $fromIndex = null): int
+{
+    $length = \count($array);
+    if (!$length) {
+        return -1;
+    }
+
+    $index = $fromIndex ?? 0;
+    if ($index < 0) {
+        $index = \min($length + $index, 0);
+    }
+
+    $iteratee = baseIteratee($predicate);
+
+    foreach ($array as $key => $value) {
+        if ($iteratee($value, $key, $array)) {
+            return $index;
+        }
+
+        $index++;
+    }
+
+    return -1;
+}
---
+++ tests/Array/FindIndexTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\findIndex;
+
+class FindIndexTest extends TestCase
+{
+    public function testFindIndex()
+    {
+        $users = [
+            ['user' => 'barney', 'active' => false],
+            ['user' => 'fred', 'active' => false],
+            ['user' => 'pebbles', 'active' => true],
+        ];
+
+        $this->assertSame(2, findIndex($users, function ($user) { return $user['user'] === 'pebbles'; }));
+        $this->assertSame(1, findIndex($users, ['user' => 'fred', 'active' => false]));
+        $this->assertSame(0, findIndex($users, ['active', false]));
+        $this->assertSame(2, findIndex($users, 'active'));
+        $this->assertSame(-1, findIndex([], 'active'));
+        $this->assertSame(2, findIndex($users, 'active', -2));
+        $this->assertSame(-1, findIndex($users, 'nothing'));
+    }
+}
