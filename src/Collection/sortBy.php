c16849042b70de8ea0238e3f2162d3d4e8dda113
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-28 20:58:39 +0200

    Add Collection.sortBy function

---
+++ src/Collection/sortBy.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+use function _\internal\baseIteratee;
+
+/**
+ * Creates an array of elements, sorted in ascending order by the results of
+ * running each element in a collection through each iteratee. This method
+ * performs a stable sort, that is, it preserves the original sort order of
+ * equal elements. The iteratees are invoked with one argument: (value).
+ *
+ * @category Collection
+ *
+ * @param array|object           $collection The collection to iterate over.
+ * @param    callable|callable[] $iteratees  The iteratees to sort by.
+ *
+ * @return array Returns the new sorted array.
+ * @example
+ * <code>
+ * $users = [
+ *   [ 'user' => 'fred',   'age' => 48 ],
+ *   [ 'user' => 'barney', 'age' => 36 ],
+ *   [ 'user' => 'fred',   'age' => 40 ],
+ *   [ 'user' => 'barney', 'age' => 34 ],
+ * ];
+ *
+ * sortBy($users, [function($o) { return $o['user']; }]);
+ * // => [['user' => 'barney', 'age' => 36], ['user' => 'barney', 'age' => 34], ['user' => 'fred', 'age' => 48], ['user' => 'fred', 'age' => 40]]
+ *
+ * sortBy($users, ['user', 'age']);
+ * // => [['user' => 'barney', 'age' => 34], ['user' => 'barney', 'age' => 36], ['user' => 'fred', 'age' => 40], ['user' => 'fred', 'age' => 48]]
+ * </code>
+ */
+function sortBy($collection, $iteratees)
+{
+    if (null === $collection) {
+        return [];
+    };
+
+    if (\is_callable($iteratees)) {
+        $iteratees = [$iteratees];
+    }
+
+    /*$length = \count($iteratees);
+    if ($length > 1 && isIterateeCall($collection, $iteratees[0], $iteratees[1])) {
+        $iteratees = [];
+    } else if ($length > 2 && isIterateeCall($iteratees[0], $iteratees[1], $iteratees[2])) {
+        $iteratees = [$iteratees[0]];
+    }*/
+
+    $result = $collection;
+
+    foreach ($iteratees as $callable) {
+        usort($result, function ($a, $b) use ($callable) {
+            $iteratee = baseIteratee($callable);
+
+            return $iteratee($a) <=> $iteratee($b);
+        });
+    }
+
+    return $result;
+}
---
+++ tests/Collection/SortByTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\sortBY;
+
+class SortByTest extends TestCase
+{
+    public function testSortBy()
+    {
+        $users = [
+            ['user' => 'fred', 'age' => 48],
+            ['user' => 'barney', 'age' => 36],
+            ['user' => 'fred', 'age' => 40],
+            ['user' => 'barney', 'age' => 34],
+        ];
+
+        $this->assertSame([['user' => 'barney', 'age' => 36], ['user' => 'barney', 'age' => 34], ['user' => 'fred', 'age' => 48], ['user' => 'fred', 'age' => 40]], sortBy($users, function ($o) { return $o['user']; }));
+        $this->assertSame([['user' => 'barney', 'age' => 34], ['user' => 'barney', 'age' => 36], ['user' => 'fred', 'age' => 40], ['user' => 'fred', 'age' => 48]], sortBy($users, ['user', 'age']));
+        $this->assertSame([], sortBy(null, []));
+    }
+}
