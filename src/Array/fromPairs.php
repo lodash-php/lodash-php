a368856019af9d09c7e9a4d7cf34d2c7fbcc5de6
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-29 14:49:48 +0200

    Add Array.fromPairs function

---
+++ src/Array/fromPairs.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * The inverse of `toPairs`, this method returns an object composed
+ * from key-value `pairs`.
+ *
+ * @category Array
+ *
+ * @param array $pairs The key-value pairs.
+ *
+ * @return object the new object.
+ * @example
+ * <code>
+ * fromPairs([['a', 1], ['b', 2]])
+ * // => stdClass(
+ * // 'a' => 1,
+ * //'b' => 2,
+ * // )
+ * </code>
+ */
+function fromPairs(array $pairs): \stdClass
+{
+    if (!\count($pairs)) {
+        return new \stdClass();
+    }
+
+    $result = new \stdClass();
+
+    foreach ($pairs as $pair) {
+        $result->{$pair[0]} = $pair[1];
+    }
+
+    return $result;
+}
---
+++ tests/Array/FromPairsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\fromPairs;
+
+class FromPairsTest extends TestCase
+{
+    public function testFromPairs()
+    {
+        $this->assertEquals((object) ['a' => 1, 'b' => 2], fromPairs([['a', 1], ['b', 2]]));
+    }
+}
