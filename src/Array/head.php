950d3b086e6cf874636e7dbdfd779357bc39e7aa
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-29 14:53:48 +0200

    Add Array.head function

---
+++ src/Array/head.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Gets the first element of `array`.
+ *
+ * @alias    first
+ * @category Array
+ *
+ * @param array $array The array to query.
+ *
+ * @return mixed Returns the first element of `array`.
+ *
+ * @see      last
+ * @example
+ * <code>
+ * head([1, 2, 3])
+ * // => 1
+ *
+ * head([])
+ * // => null
+ * </code>
+ */
+function head(array $array)
+{
+    reset($array);
+
+    return current($array) ?: null;
+}
+
+/* alias to head() */
+function first(array $array)
+{
+    return head($array);
+}
---
+++ tests/Array/HeadTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use function _\head;
+use PHPUnit\Framework\TestCase;
+
+class HeadTest extends TestCase
+{
+    public function testHead()
+    {
+        $this->assertSame(1, head([1, 2, 3]));
+        $this->assertSame(null, head([]));
+    }
+}
