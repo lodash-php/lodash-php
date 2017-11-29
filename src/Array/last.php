fa238997f998794d8d321a40f5a5f4645509dc69
Pierre du Plessis <pierre@pcservice.co.za>
2017-11-29 20:48:13 +0200

    Add Array.last function

---
+++ src/Array/last.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Gets the last element of `array`.
+ *
+ * @category Array
+ *
+ * @param array $array The array to query.
+ *
+ * @returns mixed Returns the last element of `array`.
+ * @example
+ * <code>
+ * last([1, 2, 3])
+ * // => 3
+ * </code>
+ */
+function last(array $array)
+{
+    return \end($array) ?: null;
+}
