b2e02a76cd8100b96f91704a868e4d9594f7f17b
Pierre du Plessis <pierre@pcservice.co.za>
2018-01-23 22:32:10 +0200

    Add Lang.eq function

---
+++ src/Lang/eq.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2018
+ */
+
+namespace _;
+
+/**
+ * Performs a comparison between two values to determine if they are equivalent.
+ *
+ * @param mixed $value The value to compare.
+ * @param mixed $other The other value to compare.
+ *
+ * @return boolean Returns `true` if the values are equivalent, else `false`.
+ * @example
+ * <code>
+ * $object = (object) ['a' => 1];
+ * $other = (object) ['a' => 1];
+ *
+ * eq($object, $object);
+ * // => true
+ *
+ * eq($object, $other);
+ * // => false
+ *
+ * eq('a', 'a');
+ * // => true
+ *
+ * eq(['a'], (object) ['a']);
+ * // => false
+ *
+ * eq(INF, INF);
+ * // => true
+ * </code>
+ */
+function eq($value, $other): bool
+{
+    return $value === $other;
+}
---
+++ tests/Lang/EqTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\eq;
+
+class EqTest extends TestCase
+{
+    /**
+     * @dataProvider equalValues
+     */
+    public function testIsEqual($value, $other)
+    {
+        $this->assertTrue(eq($value, $other));
+    }
+
+    /**
+     * @dataProvider notEqualValues
+     */
+    public function testIsNotEqual($value, $other)
+    {
+        $this->assertFalse(eq($value, $other));
+    }
+
+    public function notEqualValues()
+    {
+        $ob1 = new StdClass();
+        $ob1->a = 'b';
+        $ob2 = new StdClass();
+
+        $ob3 = new StdClass();
+        $ob4 = new StdClass();
+
+        return [
+            [
+                null, 'a',
+            ],
+            [
+                0, 1,
+            ],
+            [
+                [1], [],
+            ],
+            [
+                ['a' => 21], ['a' => 22],
+            ],
+            [
+                ['a' => 21], ['a' => '21'],
+            ],
+            [
+                $ob1, $ob2,
+            ],
+
+            [
+                $ob3, $ob4,
+            ],
+            [
+                new DateTime('NOW'), new DateTime('NOW'),
+            ],
+        ];
+    }
+
+    public function equalValues()
+    {
+        $date = new DateTime('NOW');
+
+        return [
+            [
+                'a', 'a',
+            ],
+            [
+                1, 1,
+            ],
+            [
+                [], [],
+            ],
+            [
+                $date, $date,
+            ],
+        ];
+    }
+}
