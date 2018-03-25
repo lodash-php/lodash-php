35dfa270cb2cee94b23487e4ffb01903a8f1950a
Pierre du Plessis <pierre@pcservice.co.za>
2018-03-25 21:08:43 +0200

    Add Collection.findLast function

---
+++ src/Collection/findLast.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+use function _\internal\baseIteratee;
+
+/**
+ * This method is like `find` except that it iterates over elements of
+ * `collection` from right to left.
+ *
+ * @category Collection
+ * @param iterable $collection The collection to inspect.
+ * @param callable $predicate The function invoked per iteration.
+ * @param int $fromIndex The index to search from.
+ * @return mixed Returns the matched element, else `undefined`.
+ * @example
+ * <code>
+ * findLast([1, 2, 3, 4], function ($n) { return $n % 2 == 1; })
+ * // => 3
+ * </cdoe>
+ */
+function findLast(iterable $collection, $predicate = null, int $fromIndex = 0)
+{
+    if (null === $predicate) {
+        $predicate = '_\identity';
+    }
+
+    $iteratee = baseIteratee($predicate);
+
+    foreach (\array_slice(\array_reverse($collection, true), $fromIndex) as $key => $value) {
+        if ($iteratee($value, $key, $collection)) {
+            return $value;
+        }
+    }
+
+    return null;
+}
---
+++ tests/Collection/FindLastTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\findLast;
+
+class FindLastTest extends TestCase
+{
+    public function testFindLast()
+    {
+        $this->assertSame(3, findLast([1, 2, 3, 4], function ($n) { return $n % 2 == 1; }));
+    }
+}
