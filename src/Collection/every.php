500cd3c6d5f870bb853f9216753e6d46113d1a21
Pierre du Plessis <pierre@pcservice.co.za>
2018-03-25 20:38:55 +0200

    Add Collection.every function

---
+++ src/Collection/every.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+use function _\internal\baseIteratee;
+
+/**
+ * Checks if `predicate` returns truthy for **all** elements of `array`.
+ * Iteration is stopped once `predicate` returns falsey. The predicate is
+ * invoked with three arguments: (value, index, array).
+ *
+ * **Note:** This method returns `true` for
+ * [empty arrays](https://en.wikipedia.org/wiki/Empty_set) because
+ * [everything is true](https://en.wikipedia.org/wiki/Vacuous_truth) of
+ * elements of empty arrays.
+ *
+ * @category Array
+ *
+ * @param array    $array     The array to iterate over.
+ * @param callable $predicate The function invoked per iteration.
+ *
+ * @return bool `true` if all elements pass the predicate check, else `false`.
+ * @example
+ * <code>
+ * every([true, 1, null, 'yes'], function ($value) { return is_bool($value);})
+ * // => false
+ *
+ * $users = [
+ *     ['user' => 'barney', 'age' => 36, 'active' => false],
+ *     ['user' => 'fred', 'age' => 40, 'active' => false],
+ * ];
+ *
+ * // The `matches` iteratee shorthand.
+ * $this->assertFalse(every($users, ['user' => 'barney', 'active' => false]));
+ * // false
+ *
+ * // The `matchesProperty` iteratee shorthand.
+ * $this->assertTrue(every($users, ['active', false]));
+ * // true
+ *
+ * // The `property` iteratee shorthand.
+ * $this->assertFalse(every($users, 'active'));
+ * //false
+ *
+ * </code>
+ */
+function every(array $array, $predicate): bool
+{
+    $iteratee = baseIteratee($predicate);
+
+    foreach ($array as $key => $value) {
+        if (!$iteratee($value, $key, $array)) {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ tests/Collection/EveryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\every;
+
+class EveryTest extends TestCase
+{
+    public function testEvery()
+    {
+        $this->assertFalse(every([true, 1, null, 'yes'], function ($value) { return is_bool($value); }));
+
+        $users = [
+            ['user' => 'barney', 'age' => 36, 'active' => false],
+            ['user' => 'fred', 'age' => 40, 'active' => false],
+        ];
+
+        // The `matches` iteratee shorthand.
+        $this->assertFalse(every($users, ['user' => 'barney', 'active' => false]));
+
+        // The `matchesProperty` iteratee shorthand.
+        $this->assertTrue(every($users, ['active', false]));
+
+        // The `property` iteratee shorthand.
+        $this->assertFalse(every($users, 'active'));
+    }
+}
