550b5cd176216897db69ade19b150a467d501209
Pierre du Plessis <pierre@pcservice.co.za>
2018-03-28 13:40:36 +0200

    Add Math.maxBy function

---
+++ src/Math/maxBy.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2018
+ */
+
+namespace _;
+
+use function _\internal\baseIteratee;
+
+/**
+ * This method is like `max` except that it accepts `iteratee` which is
+ * invoked for each element in `array` to generate the criterion by which
+ * the value is ranked. The iteratee is invoked with one argument: (value).
+ *
+ * @category Math
+ *
+ * @param array           $array    The array to iterate over.
+ * @param callable|string $iteratee The iteratee invoked per element.
+ *
+ * @return mixed Returns the maximum value.
+ * @example
+ * <code>
+ * $objects = [['n' => 1], ['n' => 2]];
+ *
+ * maxBy($objects, function($o) { return $o['n']; });
+ * // => ['n' => 2]
+ *
+ * // The `property` iteratee shorthand.
+ * maxBy($objects, 'n');
+ * // => ['n' => 2]
+ */
+function maxBy(?array $array, $iteratee)
+{
+    $iteratee = baseIteratee($iteratee);
+    $result = null;
+    $computed = null;
+
+    foreach ($array as $key => $value) {
+        $current = $iteratee($value);
+
+        if (null !== $current && (null === $computed ? ($current === $current) : $current > $computed)) {
+            $computed = $current;
+            $result = $value;
+        }
+    }
+
+    return $result;
+}
---
+++ tests/Math/MaxByTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\maxBy;
+
+class MaxByTest extends TestCase
+{
+    public function testMax()
+    {
+        $objects = [['n' => 1], ['n' => 2]];
+        $this->assertSame(['n' => 2], maxBy($objects, function ($o) { return $o['n']; }));
+        $this->assertSame(['n' => 2], maxBy($objects, 'n'));
+    }
+}
