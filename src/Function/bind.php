f65abdcb76dd13efdd64f3c8df80641df5c9dfec
Pierre du Plessis <pierre@pcservice.co.za>
2018-07-18 14:21:40 +0200

    Add Function.bind function

---
+++ src/Function/bind.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Creates a function that invokes `func` with the `this` binding of `object`
+ * and `partials` prepended to the arguments it receives.
+ *
+ * @category Function
+ *
+ * @param callable     $function The function to bind.
+ * @param object|mixed $object   The `object` binding of `func`.
+ * @param mixed[]      $partials The arguments to be partially applied.
+ *
+ * @return callable Returns the new bound function.
+ * @example
+ *
+ * function greet($greeting, $punctuation) {
+ *   return $greeting . ' ' . $this->user . $punctuation;
+ * }
+ *
+ * $object = $object = new class {
+ *     public $user = 'fred';
+ * };
+ *
+ * $bound = bind('greet', $object, 'hi');
+ * $bound('!');
+ * // => 'hi fred!'
+ */
+function bind(callable $function, $object, ...$partials): callable
+{
+    return function (...$args) use ($object, $function, $partials) {
+        $function = \Closure::fromCallable($function)->bindTo($object, $function instanceof \Closure ? $object : null);
+
+        return $function(...array_merge($partials, $args));
+    };
+}
---
+++ tests/Function/BindTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\bind;
+
+class BindTest extends TestCase
+{
+    public function testBind()
+    {
+        $object = new class
+        {
+            public $user = 'fred';
+
+            private $age = 54;
+        };
+
+        function greet($greeting, $punctuation)
+        {
+            return $greeting.' '.$this->user.$punctuation;
+        }
+
+        $bound = bind('greet', $object, 'hi');
+
+        $this->assertSame('hi fred!', $bound('!'));
+
+        $bound = bind(function ($prefix, $suffix) {
+            return $prefix.' '.$this->age.' '.$suffix;
+        }, $object, 'I\'m');
+
+        $this->assertSame('I\'m 54 years', $bound('years'));
+    }
+}
