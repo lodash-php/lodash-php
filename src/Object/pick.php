0661eee9171989a8790ccf14e4150efaf8baaf4e
Pierre du Plessis <pierre@pcservice.co.za>
2018-08-14 12:35:31 +0200

    Add Object.pick and Object.pickBy functions

---
+++ src/Object/pick.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2018
+ */
+
+namespace _;
+
+use function _\internal\basePick;
+use function _\internal\flatRest;
+
+/**
+ * Creates an object composed of the picked `object` properties.
+ *
+ * @category Object
+ *
+ * @param object          $object The source object.
+ * @param string|string[] $paths  The property paths to pick.
+ *
+ * @return \stdClass Returns the new object.
+ * @example
+ *
+ * $object = (object) ['a' => 1, 'b' => '2', 'c' => 3];
+ *
+ * pick($object, ['a', 'c']);
+ * // => (object) ['a' => 1, 'c' => 3]
+ */
+function pick($object, $paths): \stdClass
+{
+    return flatRest(function ($object, ...$paths) {
+        return basePick($object, $paths);
+    })($object, $paths);
+}
---
+++ tests/Object/PickByTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\pickBy;
+
+class PickByTest extends TestCase
+{
+    public function testPick()
+    {
+        $object = (object) ['a' => 1, 'b' => '2', 'c' => 3];
+
+        $this->assertEquals((object) ['a' => 1, 'c' => 3], pickBy($object, function ($value) { return \is_int($value); }));
+    }
+}
