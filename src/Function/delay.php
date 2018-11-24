fd85fcf755b3589a516a8e73ef7cd678f0731d1a
Pierre du Plessis <pierre@pcservice.co.za>
2018-11-24 07:35:36 +0200

    Add Function.delay function

---
+++ src/Function/delay.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Invokes `func` after `wait` milliseconds. Any additional arguments are
+ * provided to `func` when it's invoked.
+ *
+ * @category Function
+ *
+ * @param callable $func The function to delay.
+ * @param int      $wait The number of milliseconds to delay invocation.
+ * @param mixed[]  $args
+ *
+ * @return int the timer id.
+ * @example
+ * <pre>
+ * delay(function($text) {
+ *   echo $text;
+ * }, 1000, 'later');
+ * // => Echo 'later' after one second.
+ * </pre>
+ */
+function delay(callable $func, int $wait = 1, ...$args): int
+{
+    usleep($wait * 1000);
+
+    $func(...$args);
+
+    return 1;
+}
---
+++ tests/Function/DelayTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use function _\delay;
+use PHPUnit\Framework\TestCase;
+
+class DelayTest extends TestCase
+{
+    public function testDelay()
+    {
+        $a = 1;
+        $time = microtime(true);
+        delay(function ($increment) use(&$a) {
+            $a += $increment;
+        }, 20, 2);
+
+        $this->assertSame(3, $a);
+        $this->assertTrue(((microtime(true) - $time) * 1000) > 20);
+    }
+}
