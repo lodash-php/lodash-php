27d0da7b600f5f3e011179be3fd0a7e17d85015f
Pierre du Plessis <pierre@pcservice.co.za>
2018-12-10 22:41:21 +0200

    Add Function.wrap function

---
+++ src/Function/wrap.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+/**
+ * Creates a function that provides `value` to `wrapper` as its first
+ * argument. Any additional arguments provided to the function are appended
+ * to those provided to the `wrapper`.
+ *
+ * @category Function
+ *
+ * @param mixed value The value to wrap.
+ * @param callable $wrapper The wrapper function.
+ *
+ * @return callable the new function.
+ * @example
+ * <code>
+ * $p = wrap('_\escape', function($func, $text) {
+ *     return '<p>' . $func($text) . '</p>';
+ * });
+ *
+ * $p('fred, barney, & pebbles');
+ * // => '<p>fred, barney, &amp; pebbles</p>'
+ */
+function wrap($value, callable $wrapper = null): callable
+{
+    return partial($wrapper ?? '\_\identity', $value);
+}
---
+++ tests/Function/WrapTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use function _\wrap;
+use PHPUnit\Framework\TestCase;
+
+class WrapTest extends TestCase
+{
+    public function testWrap()
+    {
+        $p = wrap('_\escape', function($func, $text) {
+            return '<p>' . $func($text) . '</p>';
+        });
+
+        $this->assertSame('<p>fred, barney, &amp; pebbles</p>', $p('fred, barney, & pebbles'));
+    }
+}
