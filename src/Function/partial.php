52c23381dc028371ef7e0c24629fc9dd16a1b963
Pierre du Plessis <pierre@pcservice.co.za>
2018-12-10 22:35:51 +0200

    Add Function.partial function

---
+++ src/Function/partial.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+namespace _;
+
+use function _\internal\baseRest;
+use function _\internal\shortOut;
+
+/**
+ * Creates a function that invokes `func` with `partials` prepended to the
+ * arguments it receives.
+ *
+ * @category Function
+ *
+ * @param callable $func     The function to partially apply arguments to.
+ * @param mixed[]  $partials The arguments to be partially applied.
+ *
+ * @return callable Returns the new partially applied function.
+ *
+ * @example
+ *
+ * function greet($greeting, $name) {
+ *   return $greeting . ' ' . $name;
+ * }
+ *
+ * $sayHelloTo = partial('greet', 'hello');
+ * $sayHelloTo('fred');
+ * // => 'hello fred'
+ */
+function partial(callable $func, ...$partials): callable
+{
+    return baseRest(function ($func, $partials) {
+        $wrapper = function () use ($func, $partials) {
+            $arguments = \func_get_args();
+            $argsIndex = -1;
+            $argsLength = \func_num_args();
+            $leftIndex = -1;
+            $leftLength = \count($partials);
+            $args = [];
+
+            while (++$leftIndex < $leftLength) {
+                $args[$leftIndex] = $partials[$leftIndex];
+            }
+            while ($argsLength--) {
+                $args[$leftIndex++] = $arguments[++$argsIndex];
+            }
+
+            return $func(...$args);
+        };
+
+        return shortOut($wrapper);
+    })($func, ...$partials);
+}
+
---
+++ tests/Function/PartialTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidWorx Lodash-PHP project.
+ *
+ * @author     Pierre du Plessis <open-source@solidworx.co>
+ * @copyright  Copyright (c) 2017
+ */
+
+use PHPUnit\Framework\TestCase;
+use function _\partial;
+
+class PartialTest extends TestCase
+{
+    public function testPartial()
+    {
+        function greet($greeting, $name)
+        {
+            return $greeting.' '.$name;
+        }
+
+        $sayHelloTo = partial('greet', 'hello');
+        $this->assertSame('hello fred', $sayHelloTo('fred'));
+    }
+}
