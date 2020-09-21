9083fdc398db5594b3d6909338d8b824e661955b
Pierre du Plessis <pierre@pcservice.co.za>
2020-09-21 13:54:55 +0200

    Always include unicode constants first
    Fixes #26

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -13,6 +13,7 @@ if (file_exists($file = __DIR__.'/compiled.php')) {
     require_once $file;
 } else {
     require_once __DIR__.'/internal/Traits/CacheDataTrait.php';
+    require_once __DIR__.'/internal/unicode.php';
     require_once __DIR__.'/CacheInterface.php';
 
     foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__, RecursiveDirectoryIterator::SKIP_DOTS)) as $file) {
