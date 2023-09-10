32773f3260729a0dba77fcd0424128ff6a914d8b
sfinktah <sfinktah@github.spamtrak.org>
2023-09-10 18:11:37 +1000

    head/first aligned with lodash/underscore

--- tests/Array/HeadTest.php
+++ tests/Array/HeadTest.php
@@ -20,5 +20,26 @@ class HeadTest extends TestCase
         $this->assertSame(1, head([1, 2, 3]));
         $this->assertSame(null, head([]));
         $this->assertSame(1, first([1, 2, 3])); // Test the alias
+
+
+        # Generated from lodash in JavaScript via:
+        # $a = [ [], [1], [1, 2], "123", "1", null, false, "", {a: 'bcd'} ];
+        # $b = _.map($a, $v => _.first($v));
+        # _.each(_.zip($a, $b), function(v) {
+        #     a = JSON.stringify([v[0]]);
+        #     b = JSON.stringify([v[1]]);
+        #     a = a.substring(1, a.length - 1);
+        #     b = b.substring(1, b.length - 1);
+        #     console.log(`    $this->assertSame(${b}, ${a});`)
+        # });
+        $this->assertSame(null, []);
+        $this->assertSame(1, [1]);
+        $this->assertSame(1, [1,2]);
+        $this->assertSame("1", "123");
+        $this->assertSame("1", "1");
+        $this->assertSame(null, null);
+        $this->assertSame(null, false);
+        $this->assertSame(null, "");
+        $this->assertSame(null, ["a" => "bcd"]);
     }
 }
